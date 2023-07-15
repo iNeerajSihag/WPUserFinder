2d76ec4826237c0dc3ffa1e5a9ea140363c1c95a
iNeerajSihag <139608513+iNeerajSihag@users.noreply.github.com>
2023-07-15 12:38:10 +0530

    Improved code to work on most WordPress websites
    Added header , Session and Cookies to be seen as a real user

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import requests
 import json
 from urllib.parse import urlparse
 
-def get_user_info(website_url, user_id):
-    response = requests.get(f"{website_url}/wp-json/wp/v2/users/{user_id}")
+def get_user_info(session, website_url, user_id):
+    response = session.get(f"{website_url}/wp-json/wp/v2/users/{user_id}")
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
-def get_authors(website_url):
-    response = requests.get(f"{website_url}/wp-json/wp/v2/users")
+def get_authors(session, website_url):
+    response = session.get(f"{website_url}/wp-json/wp/v2/users")
     if response.status_code == 200:
         return response.json()
     else:
@@ -36,11 +36,17 @@ def print_and_write(file, message, end='\n'):
         file.write(message + end)
 
 def main():
+    session = requests.Session()
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    })
+
     print_and_write(None, f"+---+---+---+---+---+---+---+---+---+---+")
     print_and_write(None, "WPUserFinder - WordPress Open-Source Intelligence (OSINT) Tool")
     print_and_write(None, "Author - Neeraj Sihag")
     print_and_write(None, "GitHub - https://github.com/iNeerajSihag")
     print_and_write(None, f"+---+---+---+---+---+---+---+---+---+---+")
+
     while True:
         website_url = input("Enter URL of WordPress site (include http:// or https://). Eg - https://website.com : ")
         if validate_url(website_url):
@@ -83,7 +89,7 @@ def main():
 
     for user_id in range(1, num_users + 1):
         print_and_write(file, f"Checking for user : {user_id} --> ", end="")
-        user_info = get_user_info(website_url, user_id)
+        user_info = get_user_info(session, website_url, user_id)
         if user_info is not None:
             total_users_found += 1
             user_ids_found.append(user_id)
@@ -94,10 +100,7 @@ def main():
             print_and_write(file, f"\tUrl - {user_info['link']}")
             print_and_write(file, f"\tDescription - {user_info['description']}")
             print_and_write(file, f"\tSlug - {user_info['slug']}")
-            if 'avatar_urls' in user_info and '96' in user_info['avatar_urls']:
-                print_and_write(file, f"\tAvatar - {user_info['avatar_urls']['96']}")
-            else:
-                print_and_write(file, "\tAvatar - Not available")
+            print_and_write(file, f"\tAvatar - {user_info.get('avatar_urls', {}).get('96', 'Not available')}")
             print_and_write(file, f"\tFull Information - {json.dumps(user_info, indent=4)}")
         else:
             print_and_write(file, "User not found")
@@ -112,7 +115,7 @@ def main():
     print_and_write(file, f"-------------------------------------")
 
     print_and_write(file, "\nChecking for authors...\n")
-    authors = get_authors(website_url)
+    authors = get_authors(session, website_url)
     if authors is not None and len(authors) > 0:
         print_and_write(file, f"Total authors found: {len(authors)}")
         for author in authors:
